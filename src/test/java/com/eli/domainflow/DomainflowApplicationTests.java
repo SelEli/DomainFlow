ebc926c85c7eb87128c65b887029c82f0b2fe4e8
SelEli <seleli@live.fr>
2026-02-14 22:34:59 +0100

    Initial commit DomainFlow

---
+++ src/test/java/com/eli/domainflow/DomainflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eli.domainflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
